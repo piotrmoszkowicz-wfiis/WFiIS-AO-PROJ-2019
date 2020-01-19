4f727ee455aafdffa979c7386a45927f84045c32
Piotr Moszkowicz <piotr@moszkowicz.pl>
2020-01-19 20:02:06 +0100

    Add new images, first version of project

---
+++ proj.m
@@ -0,0 +1,49 @@
+clear; clc;
+
+imds = imageDatastore('images', 'LabelSource', 'foldernames', 'IncludeSubfolders',true);
+
+numTrainFiles = 15;
+
+[imdsTrain,imdsValidation] = splitEachLabel(imds,numTrainFiles,'randomize');
+
+layers = [
+    imageInputLayer([600 800 3])
+    
+    convolution2dLayer(3,8,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    maxPooling2dLayer(2,'Stride',2)
+    
+    convolution2dLayer(3,16,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    maxPooling2dLayer(2,'Stride',2)
+    
+    convolution2dLayer(3,32,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    fullyConnectedLayer(2)
+    softmaxLayer
+    classificationLayer
+];
+
+options = trainingOptions('sgdm', ...
+    'ExecutionEnvironment','cpu', ... # suggestion to change to gpu or paralell, if you have suitable system
+    'InitialLearnRate',0.01, ...
+    'MaxEpochs',10, ...
+    'Shuffle','every-epoch', ...
+    'ValidationData',imdsValidation, ...
+    'ValidationFrequency',30, ...
+    'Verbose',false, ...
+    'Plots','training-progress');
+
+net = trainNetwork(imdsTrain,layers,options);
+
+YPred = classify(net,imdsValidation);
+YValidation = imdsValidation.Labels;
+
+accuracy = sum(YPred == YValidation)/numel(YValidation);
+
